67381932a377700aadc4a880067cf2a7106b38ef
marble-git <516018505@qq.com>
2021-08-25 09:21:56 +0800

    change biqugepy

--- 03dataextraction/biquge.py
+++ 03dataextraction/biquge.py
@@ -8,6 +8,7 @@
 
 import lxml
 import time
+import os
 import requests
 from lxml import etree
 from collections import namedtuple
@@ -15,6 +16,11 @@ from collections import namedtuple
 NovelMeta = namedtuple('NovelMeta', 'name author intro')
 NovelState = namedtuple('NovelState', 'lastupdate lastchapter')
 NovelChapter = namedtuple('Chapter', 'title chapter_url')
+ChapterContent = namedtuple('ChapterContent', 'title content')
+
+
+def writenamedtuple(ntuple):
+    return '\n'.join(ntuple)
 
 
 class Novel:
@@ -50,14 +56,53 @@ class Novel:
         self.novelmeta = NovelMeta(novel_name, novel_author, novel_introducton)
         self.novelstate = NovelState(last_updated_time, last_updated_chapter)
 
-    def parse_detail(self,detail_url):
-        pass
+    def parse_detail(self, detail_url):
+        response = requests.get(detail_url, headers=self.headers)
+        text = response.content.decode('utf-8')
+        html = etree.HTML(text)
+        title = html.xpath('//h1/text()')[0]
+        print(title)
+        chapter_content = html.xpath("//div[@id='content']/text()")
+        chapter_content = [string.strip('\xa0') for string in chapter_content[:]]
+        chapter_content = '\n'.join(chapter_content)
+        return ChapterContent(title, chapter_content)
 
     def action(self):
         self.parse_page()
+        self.updated()
+        for chapter in self.chapters:
+            c = self.parse_detail(chapter.chapter_url)
+            # print(chapter.title)
+            # print(c.title == chapter.title)
+            # print(c)
+            break
 
-    def pre_check(self):
-        pass
+    def updated(self):
+        if not os.path.isdir(self.novelmeta.name):
+            os.mkdir(self.novelmeta.name)
+            print('directory <{}> created.'.format(self.novelmeta.name))
+        if not os.path.exists(path := os.path.join(self.novelmeta.name, 'novelmeta.txt')):
+            with open(path, 'wt', encoding='utf-8') as f:
+                f.write(writenamedtuple(self.novelmeta))
+                print(f'<{path}> created.')
+        if not os.path.exists(path := os.path.join(self.novelmeta.name, 'novelstate.txt')):
+            with open(path, 'wt', encoding='utf-8') as f:
+                f.write(writenamedtuple(self.novelstate))
+                print(f'<{path}> created.')
+
+        with open(os.path.join(self.novelmeta.name,'novelstate.txt')) as f:
+            text = f.readlines()
+        text = [line.strip('\n') for line in text]
+        laststate = NovelState(text[0],text[1])
+        print(laststate)
+        if self.novelstate == laststate:
+            print('not updated')
+            # return False
+        self.existing_chapters = os.listdir(self.novelmeta.name)
+        # self.existing_chapters = [chapter if ]
+        print(self.existing_chapters)
+        print('updated')
+        return True
 
 
 if __name__ == "__main__":
